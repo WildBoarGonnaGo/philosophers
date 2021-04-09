aa17197d4267cd1018a31239a5e3f198a537f0d8
WildBoarGonnaGo <60565993+WildBoarGonnaGo@users.noreply.github.com>
2021-04-09 19:22:21 +0300

    "philo_three: base files"

---
+++ philo_three/main.c
@@ -0,0 +1,20 @@
+/*#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/types.h>
+*/
+
+#include "philo_three.h"
+
+int main(int argc, char *argv[])
+{
+    if (argc < 5 && argc > 6)
+    {
+        printf("Error: wrong number of arguments\n");
+        return (1);
+    }
+    return (0);
+}
